ac8e5b5854ed70e274fd1a772a793221f0dc88ce
Ronald803 <ronaldblancobalboa@gmail.com>
2023-06-12 23:25:23 -0400

    erasing swagger configuration because the vercel server where this aplication was deployed has failed

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 const config = require('./config');
 const router = require('./network/routes')
 const connectDB = require('./db')
-const swaggerUi = require('swagger-ui-express')
-const YAML = require('yamljs')
+//const swaggerUi = require('swagger-ui-express')
+//const YAML = require('yamljs')
 
 // config for documentation with swagger
-const swaggerDocument = YAML.load('./docs.yaml');
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
+//const swaggerDocument = YAML.load('./docs.yaml');
+//app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 
 
 connectDB(config.dbUrl);
